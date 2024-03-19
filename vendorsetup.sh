7e109b4a3804b7888a4734b93dffcc76b8e88136
afterallafk <shivamatiet2001@gmail.com>
2024-03-19 12:10:33 +0530

    veux: Setup vendorsetup for auto cloning
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/afterallafk/vendor_xiaomi_veux.git -b 14 --depth=1 vendor/xiaomi/veux
+git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6375.git -b fourteen --depth=1 kernel/xiaomi/sm6375
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_miuicamera-veux.git -b fourteen --depth=1 vendor/xiaomi/miuicamera-veux
+git clone https://github.com/AswinOP/device_xiaomi_veux-kernel.git -b thirteen device/xiaomi/veux-kernel
