07d2008292e3d9883cdf16ca7042156b62b47182
afterallafk <shivamatiet2001@gmail.com>
2024-03-19 11:50:34 +0530

    Project Blaze Bringup !!!
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- blaze_veux.mk
+++ blaze_veux.mk
@@ -11,18 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Project Blaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
+# Blaze Stuff
+BLAZE_BUILD_TYPE := UNOFFICIAL
+BLAZE_MAINTAINER := ＡＤＩＴＹＡ
+TARGET_SUPPORTS_BLUR := true
+WITH_GAPPS := true
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2201116SG
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := blaze_veux
 PRODUCT_SYSTEM_NAME := veux_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
